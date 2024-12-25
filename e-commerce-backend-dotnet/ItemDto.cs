26921d8bbc29fd91773cd386ed9497bf52c499c3
Xenko10 <kubinios_2002@o2.pl>
2024-12-25 07:02:22 +0100

    first commit

---
+++ e-commerce-backend-dotnet/ItemDto.cs
@@ -0,0 +1,8 @@
+﻿namespace e_commerce_backend_dotnet;
+
+public class Item(int Id, string Name)
+{
+    public int Id { get; set; } = Id;
+    public string Name { get; set; } = Name;
+
+}
---
+++ e-commerce-backend-dotnet/Program.cs
@@ -0,0 +1,27 @@
+using e_commerce_backend_dotnet;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/item", () =>
+{
+    var item = new Item(Id: 1, Name: "isana");
+    item.Name = "lol";
+    return item;
+});
+
+app.MapGet("/items", () =>
+{
+    var items = new List<Item>
+    {
+        new Item(1, "isana"),
+        new Item(2, "dove"),
+        new Item(3, "nivea")
+    };
+    return items;
+});
+
+app.Run();
