72f3f4df7f57cf1529aaa733db1e29a297d4054f
Jakub Kołaczyński <kubinios_2002@o2.pl>
2025-01-03 18:10:41 +0100

    use DbContextPool instead

--- src/Program.cs
+++ src/Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
+builder.Services.AddDbContextPool<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection")));
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
